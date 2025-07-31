5dd0880db9d994efad8417f2ba6876ca32de2021
Rafaela <rafaelabezerra133@gmail.com>
2025-07-31 14:55:25 -0300

    feat: configura main.py para rodar bot e registrar handlers

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+
+from controllers.bot_controller import start, help_command, handle_message
+
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    print("🤖 Bot rodando...")
+    app.run_polling()
